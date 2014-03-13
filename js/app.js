96c1cb7330c31791d359f5057192dca2878d5e3c
thfield <thfield@gmail.com>
2014-03-13 13:25:57 -0700

    separate animation buttons

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
     function test() {
         $("#test").toggleClass("tested");
         // test function goes here, activated by "test" button
-        animateSwap();
+       
     };
 
-    function test2() {
-        reverseList();
+    function testanimate() {
+        animateSwap();
+
     };
     var itemHTML = '<div class="portlet"><div class="portlet-header"></div><div class="portlet-content"><div class="displayContent"></div><textarea class="editContent hidden"></textarea></div></div>'
     var itemHTMLedit = '<div class="portlet"><div class="portlet-header"></div><div class="portlet-content"><div class="displayContent hidden"></div><textarea class="editContent"></textarea></div></div>'
@@ -21,21 +22,21 @@ $(document).ready(function () {
     });
 
     //
-    // called on scriptload to copy value from .edit to .display
+    // called on scriptload to copy values from .edit to .display
     //
     $('.portlet-content').each(function () {
         $foo = $(this).children('.displayContent').text();
         $(this).children('.editContent').val($foo);
     });
-    
+
     //
     // button-function bindings
     //
     $(document).on('click', '#tester', function () {
         test();
     });
-    $(document).on('click', '#tester2', function () {
-        test2();
+    $(document).on('click', '#testanimate', function () {
+        animateSwap();
     });
     $(document).on('click', '#reverse', function () {
         reverseList();
@@ -56,13 +57,16 @@ $(document).ready(function () {
     $(document).on('click', '#edit', function () {
         editMode();
     });
-    
+    $(document).on('click', '#editanimate', function () {
+        editModeAnimate();
+    });
+
     //
     // control functions      
     //
-    
+
     var activeEdit = false;
-    
+
     function reverseList() {
         var list = $(".portlets>.portlet").get().reverse();
         $(".portlets").empty();
@@ -73,7 +77,10 @@ $(document).ready(function () {
 
     function animateSwap() {
         var list = $(".portlets>.portlet").get();
-        $(list).toggle("slide", 600);
+        $(list).fadeOut(400, function () {
+            reverseList();
+            $(list).fadeIn(400);
+        });
     };
 
     function deleteall() {
@@ -81,7 +88,7 @@ $(document).ready(function () {
     };
 
     function newentry() {
-        if ( activeEdit == true) {
+        if (activeEdit == true) {
             $(".portlets").append(itemHTMLedit);
         } else {
             $(".portlets").append(itemHTML);
@@ -90,18 +97,34 @@ $(document).ready(function () {
 
     function emptytrash() {
         $(".trashcan").children('.portlet').remove();
-    };    
+    };
 
-    function editMode() {
+    function editModeAnimate() {
         activeEdit = !activeEdit;
-        $('.portlet-content').each(function () {
+        var $bar = $('.portlet-content');
+        $bar.each(function () {
             $foo = $(this).children('.editContent').val();
             $(this).children('.displayContent').text($foo);
         });
-        $('.editContent').toggleClass("hidden");
-        $('.displayContent').toggleClass('hidden');
+        $bar.fadeOut(400, function () {
+            $bar.children('.editContent').toggleClass("hidden");
+            $bar.children('.displayContent').toggleClass('hidden');
+            $bar.fadeIn(400)
+        });
     };
-
+    
+    function editMode() {
+        activeEdit = !activeEdit;
+        var $bar = $('.portlet-content');
+        $bar.each(function () {
+            $foo = $(this).children('.editContent').val();
+            $(this).children('.displayContent').text($foo);
+        });
+       
+            $bar.children('.editContent').toggleClass("hidden");
+            $bar.children('.displayContent').toggleClass('hidden');
+            
+    };
 
 
 });
