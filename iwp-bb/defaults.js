55f3d0adafbf3ac48cf0bdb45ce7e0459cb8a680
thfield <thfield@gmail.com>
2015-04-11 17:56:03 -0700

    forgot file

---
+++ iwp-bb/defaults.js
@@ -0,0 +1,22 @@
+var defaults = [   {
+        "title": "Card 1",
+        "content": "Toggle display/edit mode by clicking on the pencil"
+    }, {
+        "title": "Card 2",
+        "content": "Reverse the stack of cards by clicking on the cycle arrows"
+    }, {
+        "title": "Card 3",
+        "content": "Edit a specific card by double clicking on its text or clicking on the pencil in its header"
+    }, {
+        "title": "Card 4",
+        "content": "Add a card to the bottom/delete the bottom card by clicking on the plus/minus"
+    }, {
+        "title": "Card 7",
+        "content": "Move a card to the trash by clicking the X in the upper right"
+    }, {
+        "title": "Card 8",
+        "content": "Empty the trash by clicking the 'Trash' button"
+    }, {
+        "title": "Card 9",
+        "content": "Delete all cards by clicking the X inside the trashcan"
+    }];
